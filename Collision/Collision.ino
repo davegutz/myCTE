9e520dce139fae273aa44aa925a33f0bb3abcb89
Dave G <davegutz@alum.mit.edu>
2024-08-12 03:50:11 -0400

    update

--- Collision/Collision.ino
+++ Collision/Collision.ino
@@ -14,7 +14,25 @@
   This example code is in the public domain.
 */
 
-#include <Arduino_LSM6DS3.h>
+#define USE_ARDUINO
+
+#ifdef USE_ARDUINO
+  #include <Arduino.h> //needed for Serial.println
+  #include <Arduino_LSM6DS3.h>
+#else
+  #error "Only Arduino nano 33 iot has built in IMU"
+  #include "application.h"  // Particle
+#endif
+
+// Dependent includes.   Easier to sp.debug code if remove unused include files
+#include "Sync.h"
+
+#define TALK_DELAY            313UL     // Talk wait, ms (313UL = 0.313 sec)
+#define READ_DELAY             10UL     // Sensor read wait, ms (10UL = 0.01 sec) Dr
+#define CONTROL_DELAY         100UL     // Control read wait, ms (100UL = 0.1 sec)
+#define PLOT_DELAY            100UL     // Plot wait, ms (100UL = 0.1 sec)
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -47,28 +65,58 @@ void setup() {
 
 void loop()
 {
+  // Synchronization
+  static unsigned long long now = (unsigned long long) millis();
+  now = (unsigned long long) millis();
+  boolean chitchat = false;
+  static Sync *Talk = new Sync(TALK_DELAY);
+  boolean read = false;
+  static Sync *ReadSensors = new Sync(READ_DELAY);
+  boolean updating_plots;
+  static Sync *Plotting = new Sync(PLOT_DELAY);
+  boolean control;
+  static Sync *ControlSync = new Sync(CONTROL_DELAY);
+  unsigned long long elapsed = 0;
+  static boolean reset = true;
+  static unsigned long long start = millis();
 
-  float a = 0;
-  float b = 0;
-  float c = 0;
-  float x = 0;
-  float y = 0;
-  float z = 0;
+  static float a = 0;
+  static float b = 0;
+  static float c = 0;
+  static float x = 0;
+  static float y = 0;
+  static float z = 0;
   boolean gyro_ready = false;
   boolean accel_ready = false;
 
-  if (IMU.accelerationAvailable())
+
+  ///////////////////////////////////////////////////////////// Top of loop////////////////////////////////////////
+
+  // Synchronize
+  read = ReadSensors->update(millis(), reset);
+  chitchat = Talk->update(millis(), reset);
+  elapsed = ReadSensors->now() - start;
+  control = ControlSync->update(millis(), reset);
+  updating_plots = Plotting->update(millis(), reset);
+
+
+
+  // Read sensors
+  if ( read )
   {
-    IMU.readAcceleration(x, y, z);
-    accel_ready = true;
-  }
-  if (IMU.gyroscopeAvailable())
-  {
-    IMU.readGyroscope(a, b, c);
-    gyro_ready = true;
+    if (IMU.accelerationAvailable())
+    {
+      IMU.readAcceleration(x, y, z);
+      accel_ready = true;
+    }
+    if (IMU.gyroscopeAvailable())
+    {
+      IMU.readGyroscope(a, b, c);
+      gyro_ready = true;
+    }
   }
 
-  if (accel_ready || gyro_ready)
+  if (updating_plots)
   {
     Serial.print(a);
     Serial.print('\t');
@@ -85,6 +133,8 @@ void loop()
 
   gyro_ready = false;
   accel_ready = false;
-  delay(100);
 
-}
+  // Initialize complete once sensors and models started and summary written
+  if ( read ) reset = false;
+
+}  // loop
